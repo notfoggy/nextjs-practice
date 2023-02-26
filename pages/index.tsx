f37b24ad284c170b38f0a0cfeb7ddff75d32c855
jinwoo <notfoggy2@gmail.com>
2023-02-26 22:34:56 +0900

    getStaticProps 설정완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,50 @@
-import Head from "next/head";
-import Image from "next/image";
-import homeStyles from "../styles/Home.module.css";
+import { GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import { getSortedPostsData } from '../lib/posts';
+import homeStyles from '../styles/Home.module.css';
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Kim Jinwoo</title>
-      </Head>
-      <section className={homeStyles.headingMd}>
-        <p>[Kim jw Intoduction]</p>
-        <p>(This is a website)</p>
-      </section>
-      <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
-        <h2>Blog</h2>
-        <ul></ul>
-      </section>
-    </div>
-  );
-}
+const Home = ({
+	allPostsData,
+}: {
+	allPostsData: {
+		date: string;
+		title: string;
+		id: string;
+	}[];
+}) => {
+	return (
+		<div>
+			<Head>
+				<title>Kim Jinwoo</title>
+			</Head>
+			<section className={homeStyles.headingMd}>
+				<p>[Kim jw Intoduction]</p>
+				<p>(This is a website)</p>
+			</section>
+			<section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
+				<h2>Blog</h2>
+				<ul>
+					{allPostsData.map(({ id, title, date }) => (
+						<li className={homeStyles.listItem} key={id}>
+							<a>{title}</a>
+							<br />
+							<small className={homeStyles.lightText}>{date}</small>
+						</li>
+					))}
+				</ul>
+			</section>
+		</div>
+	);
+};
+
+export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+};
